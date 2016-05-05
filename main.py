de21fd2401074cb4bb20b4ca5867b756ffb211e7
Christian Martin Samuelsen <chrimsam@student.matnat.uio.no>
2016-05-05 12:14:05 +0200

    Added endpoint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello():
     return "CoverVote!"
 
+@app.route("/addsong")
+def addsong():
+	return render_template("SuggestSong.html")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
