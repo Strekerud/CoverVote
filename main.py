89d8c0dfb1a3ba6ca384166f15618933569c3a3b
kristr <kristr@knowit.no>
2016-05-05 14:24:37 +0200

    Index first commit

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "CoverVote!"
+def index():
+    return render_template("index.html")
 
 @app.route("/addsong")
 def addsong():
