21992731d8a9d5b21577eebe6a732dca94989bdb
kristr <kristr@knowit.no>
2016-05-05 15:49:24 +0200

    ADDED F

--- mongomanager.py
+++ mongomanager.py
@@ -1,9 +1,9 @@
 #/usr/bin/python
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://covervote:covervote12@ds013202.mlab.com:13202/heroku_w60qmjw')
+client = MongoClient('mongodb://covervote:covervote12@ds013202.mlab.com:13202/heroku_w60qmjwf')
 #LOKAL: client = MongoClient()
-db = client.heroku_w60qmjw
+db = client.heroku_w60qmjwf
 collection = db.tracks
 
 def add_track_to_db(track_json):
