4fa54d3bc805890f5833b09a35ebf82add20fc52
beezlebob <emil.strom90@gmail.com>
2016-06-06 11:19:42 +0200

    søk med artist fungerer, var bug hvis man får opp mindre enn 3 alternativer

--- spotifycommunicator.py
+++ spotifycommunicator.py
@@ -12,9 +12,9 @@ def search_tracks(trackname,artist=""):
     top_three_search_results = []
     top_three_search_results.append(choose_search_result(spotify_search_result_list[0]))
     if len(spotify_search_result_list)>1:
-        top_three_search_results.append(choose_search_result(spotify_search_result_list[2]))
+        top_three_search_results.append(choose_search_result(spotify_search_result_list[1]))
         if len(spotify_search_result_list)>2:
-               top_three_search_results.append(choose_search_result(spotify_search_result_list[1]))
+               top_three_search_results.append(choose_search_result(spotify_search_result_list[2]))
                
     return top_three_search_results
 
