3a7b614e5e2031cd3c16aa9e32c447158e624c3e
beezlebob <emil.strom90@gmail.com>
2016-06-06 10:18:31 +0200

    la inn suggest_song i modal ;)

--- static/js/coverVote.js
+++ static/js/coverVote.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-    $("#Options").find('tr').click(function(){
+    $(".Options").find('tr').click(function(){
 	window.location = $(this).index();
     });
-    $(".submit_form").hide();
     $("#addsong").click(function(){
+	$(".Options").hide();
+	$("#SuggestModal").modal();
 	$(".submit_form").show();
 	$("#addsong").hide();
     });
-    if($("#SuggestModal").find("tr")[1]){
+    if($(".Options").find("tr")[1]){
+	$(".Options").show();
 	$("#SuggestModal").modal();
     }
 });    
